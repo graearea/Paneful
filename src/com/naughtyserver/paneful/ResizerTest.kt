b6a30248f0cce57d859ae7f61300ab4c33ac6fb4
JaeRae <john.rae@springer.com>
2017-01-20 16:14:57 +0000

    reorder tests

--- src/com/naughtyserver/paneful/ResizerTest.kt
+++ src/com/naughtyserver/paneful/ResizerTest.kt
@@ -9,49 +9,50 @@ import org.junit.Test
 
 class ResizerTest{
 
+    private val minSize = 0.3f
+    private val maxSize = 0.7f
 
     @Test
    fun shouldGrowLeftPane(){
-        assertEquals(Resizer.movePane(GROW, LEFT_PANE, 0.3f), 0.4f)
+        assertEquals(Resizer.movePane(GROW, LEFT_PANE, minSize), 0.4f)
         assertEquals(Resizer.movePane(GROW, LEFT_PANE, 0.5f), 0.6f)
     }
 
     @Test
-    fun shouldNotGrowLeftPane(){
-        assertEquals(Resizer.movePane(GROW, LEFT_PANE, 0.7f), 0.7f)
-    }
-
-    @Test
-    fun shouldNotShrinkLeftPane(){
-        assertEquals(Resizer.movePane(SHRINK, LEFT_PANE, 0.3f), 0.3f)
-    }
-
-    @Test
     fun shouldShrinkLeftPane(){
-        assertEquals(Resizer.movePane(SHRINK, LEFT_PANE, 0.3f), 0.3f)
         assertEquals(Resizer.movePane(SHRINK, LEFT_PANE, 0.5f), 0.4f)
-        assertEquals(Resizer.movePane(SHRINK, LEFT_PANE, 0.7f), 0.6f)
+        assertEquals(Resizer.movePane(SHRINK, LEFT_PANE, maxSize), 0.6f)
+    }
+
+    @Test
+    fun shouldGrowRightPane(){
+        assertEquals(Resizer.movePane(GROW,RIGHT_PANE, 0.5f), 0.4f)
+        assertEquals(Resizer.movePane(GROW,RIGHT_PANE, maxSize), 0.6f)
     }
 
     @Test
    fun shouldShrinkRightPane(){
-        assertEquals(Resizer.movePane(SHRINK,RIGHT_PANE, 0.3f), 0.4f)
+        assertEquals(Resizer.movePane(SHRINK,RIGHT_PANE, minSize), 0.4f)
         assertEquals(Resizer.movePane(SHRINK,RIGHT_PANE, 0.5f), 0.6f)
     }
 
     @Test
-    fun shouldGrownRightPane(){
-        assertEquals(Resizer.movePane(GROW,RIGHT_PANE, 0.5f), 0.4f)
-        assertEquals(Resizer.movePane(GROW,RIGHT_PANE, 0.7f), 0.6f)
+    fun shouldNotGrowLeftPane(){
+        assertEquals(Resizer.movePane(GROW, LEFT_PANE, maxSize), maxSize)
+    }
+
+    @Test
+    fun shouldNotShrinkLeftPane(){
+        assertEquals(Resizer.movePane(SHRINK, LEFT_PANE, minSize), minSize)
     }
 
     @Test
    fun shouldNotShrinkRightPane(){
-        assertEquals(Resizer.movePane(SHRINK,RIGHT_PANE, 0.7f), 0.7f)
+        assertEquals(Resizer.movePane(SHRINK,RIGHT_PANE, maxSize), maxSize)
     }
 
     @Test
     fun shouldNotGrownRightPane(){
-        assertEquals(Resizer.movePane(GROW,RIGHT_PANE, 0.3f), 0.3f)
+        assertEquals(Resizer.movePane(GROW,RIGHT_PANE, minSize), minSize)
     }
 }
